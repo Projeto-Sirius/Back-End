9f4fe03bd86bb499c05573d1e85bdd6d6a66c7c4
alexamorim17 <94928551+alexamorim17@users.noreply.github.com>
2022-01-26 10:56:01 -0300

    Adicionando anotações
    Adicionando anotações

--- Sirius/src/main/java/br/com/sirius/sirius/Controller/CategoriaController.java
+++ Sirius/src/main/java/br/com/sirius/sirius/Controller/CategoriaController.java
@@ -19,49 +19,50 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.sirius.sirius.Model.Categoria;
 import br.com.sirius.sirius.Repository.CategoriaRepository;
 
-@RequestMapping("/categoria")
-@RestController
-@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RequestMapping("/categoria")//Gera o EndPoint 
+@RestController//Cria um Controller
+@CrossOrigin(origins = "*", allowedHeaders = "*") // Permite que o Front-End se comunique com o Back-End
 public class CategoriaController {
 	
 	
-	@Autowired
+	@Autowired // Faz a injeção das dependências na Interface
 	CategoriaRepository repository;
 	
-	@GetMapping
-	ResponseEntity <List<Categoria>> getAll(){
-		return ResponseEntity.ok(repository.findAll());
+	@GetMapping // Responde com todos os dados de Categoria
+	ResponseEntity <List<Categoria>> getAll(){ 
+		return ResponseEntity.ok(repository.findAll()); //Abstrai todos os dados de Categoria
 	}
 	
-	@GetMapping("/{id}")
-	ResponseEntity <Categoria> getById(@PathVariable Long id){
+	@GetMapping("/{id}")// Responde com todos os dados que contem o id
+	ResponseEntity <Categoria> getById(@PathVariable Long id){ //Procura o id atravez da url
 		return repository.findById(id)
 				.map(resp -> ResponseEntity.ok(resp))
-				.orElse(ResponseEntity.notFound().build());
+				.orElse(ResponseEntity.notFound().build()); // Devolve todos os dados com o id
 	}
 	
-	@GetMapping("/descricao/{descricao}")
-	ResponseEntity <List<Categoria>> getByDescricao(@PathVariable String descricao){
-		return ResponseEntity.ok(CategoriaRepository.findAllByDescricaoContainingIgnoreCase(descricao));
+	@GetMapping("/descricao/{descricao}") // Procura pela descrição no Banco de dados
+	ResponseEntity <List<Categoria>> getByDescricao(@PathVariable String descricao){ //abstrai a descricao apartir da url
+		return ResponseEntity.ok(CategoriaRepository.findAllByDescricaoContainingIgnoreCase(descricao)); //Procura no BD atravez da descricao
 	}
 	
-	@PostMapping
+	
+	@PostMapping // Envia as informações para o Banco de dados
 	ResponseEntity <Categoria> post (@RequestBody @Validated Categoria categoria){
 		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(categoria));
 		
 		
 	}
 	
-	@PutMapping
-	ResponseEntity <Categoria> put (@RequestBody @Validated Categoria categoria){
-		return repository.findById(categoria.getId())
-				.map(resp -> ResponseEntity.ok().body(repository.save(categoria)))
+	@PutMapping // Alterar dados na tabela categoria
+	ResponseEntity <Categoria> put (@RequestBody @Validated Categoria categoria){// Faz a alteração a apartir do corpo da requisição
+		return repository.findById(categoria.getId()) //Procurar pelo Id
+				.map(resp -> ResponseEntity.ok().body(repository.save(categoria))) //Caso exista ele modifica os dados
 				.orElse(ResponseEntity.notFound().build());
 	}
 	
-	@DeleteMapping("/{id}")
-	ResponseEntity<?> deleteCategoria (@PathVariable Long id){
-		return repository.findById(id)
+	@DeleteMapping("/{id}") //Deletar dados do BD atravez do ID
+	ResponseEntity<?> deleteCategoria (@PathVariable Long id){ 
+		return repository.findById(id) //Procura pelo Id
 				.map(resp ->{
 					repository.deleteById(id);
 					return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
